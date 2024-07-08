8e0545b5e121682639d61012a4b141771adc66d0
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-07-08 15:35:10 -0300

    operações básicas, lista de tarefas

---
+++ src/list/operacoesbasicas/ListaTarefa.java
@@ -0,0 +1,46 @@
+package list.operacoesbasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t: tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        listaTarefa.adicionarTarefa("Tarefa 01");
+        listaTarefa.adicionarTarefa("Tarefa 02");
+        listaTarefa.adicionarTarefa("Tarefa 02");
+
+        listaTarefa.removerTarefa("Tarefa 01");
+        System.out.println(listaTarefa.obterTotalTarefas());
+        listaTarefa.obterDescricoesTarefas();
+    }
+}
