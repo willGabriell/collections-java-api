c2d66005fbebfd8bffd1429bed8b3c9df50f9775
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-07-09 17:17:08 -0300

    Desafio Conjunto Convidados

---
+++ src/set/ConjuntoConvidados.java
@@ -0,0 +1,38 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados
+{
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigo(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c: convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            } else {
+                System.out.println("Código não encontrado");
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+}
