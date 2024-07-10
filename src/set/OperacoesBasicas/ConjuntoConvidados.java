d0ae1dd6a8b682857a8e1fe49a0be622451cf45c
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-07-10 13:50:12 -0300

    Mudanças na organização dos arquivos

--- src/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -1,4 +1,4 @@
-package set;
+package set.OperacoesBasicas;
 
 import java.util.HashSet;
 import java.util.Set;
--- src/set/OperacoesBasicas/Palavra.java
+++ src/set/OperacoesBasicas/Palavra.java
@@ -1,4 +1,4 @@
-package set;
+package set.OperacoesBasicas;
 
 public class Palavra
 {
